54fd07edaa473244fcbdb44974abded3cf34c19b
Douglas <douglasagbeve@yahoo.co.uk>
2019-02-20 16:38:50 +0100

    Setting up

---
+++ cmake-build-debug/CMakeFiles/mySite.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mySite.dir/main.cpp.o"
+  "mySite.pdb"
+  "mySite"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mySite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/mySite.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mySite.
+# This may be replaced when dependencies are built.
